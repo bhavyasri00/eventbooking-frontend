c96f3c0882d141fa1405e868755b38129123a2d5
bhavyasri00 <alavalabhavyanagasri390@gmail.com>
2025-11-25 15:41:12 +0530

    1

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  "https://eventbooking-backend-nmlq.onrender.com";
+  "https://eventbooking-backend-nmlq.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
